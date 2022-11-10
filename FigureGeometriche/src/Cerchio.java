e22d531766599c67260891ab3534a1540a757f8d
iltawa <minattalh01@gmail.com>
2022-11-10 12:00:43 +0100

    modifica a riga 11

--- FigureGeometriche/src/Cerchio.java
+++ FigureGeometriche/src/Cerchio.java
@@ -8,7 +8,7 @@ public class Cerchio extends Forma {
 
     @Override
     public double area() {
-        return raggio * raggio * Math.PI;
+        return raggio * raggio * Math.PI; //questa è una modifica
     }
 
     public double getRaggio() {
